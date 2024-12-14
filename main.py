91fbfe8a1e20aeebf2ee3a7d2e64f6858b7a2ece
AndreasPascalis017 <andreaspascalis11@gmail.com>
2024-12-14 10:55:58 +0700

    update code kasar

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import cv2
-import time
+import cv2      ## Import library yang akan digunakan.
+## cv2 digunakan untuk pengolahan gambar dan video dalam proyek.
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) ## fungsi untuk membuka kamera default (webcam).
 
-threshold = 1
-ret, temp = cap.read()
-lasth = 0
+threshold = 1  ## mengambil 1 piksel yang akan digunakan sebagai iteras (masih akan dicoba lebih lanjut niali yang sesuai)
+ret, temp = cap.read()  ## setelah membaca 1 piksel disimpan sementara di temp.
+lasth = 0  ## variabel untuk melacak baris yang sedang diproses (semua nama variabel masih berupa nama acak mungkin ada yang diubah ada yang tidak).
 newing = temp.copy()
 upsidedown = False
 
 print(temp.shape)
 while True:
-    ret, img = cap.read()
+    ret, img = cap.read()  ## setelah membaca frame disimpan dalam img.
     img = cv2.flip(img, int(not upsidedown))
     if (type(img) == type(None)):
         break
@@ -20,13 +20,15 @@ while True:
     newing[0: lasth] = temp[0: lasth]
     newing[lasth:-1] = img[lasth:-1]
     cv2.line(newing, (0, lasth), (640, lasth),
-             (0, 255, 0), thickness=2)
+             (0, 255, 0), thickness=2)  ## untuk membuat garis penanda distorsi (hijau)
     lasth += threshold
-    cv2.imshow('video', newing)
+    cv2.imshow('video', newing)  ## menampilkan video dalam window.
     if cv2.waitKey(33) == 27:
         break
     if lasth >= img.shape[1]:
         break
 
-cv2.imwrite('image.png', newing)
-cv2.destroyAllWindows()
+cv2.imwrite('image.png', newing)  ## file disimpan dengan nama image.png
+cv2.destroyAllWindows()  
+
+## update terakhir tgl 14-12-2024
